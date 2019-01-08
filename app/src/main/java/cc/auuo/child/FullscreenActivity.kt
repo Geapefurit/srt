1581338b23cfcd0470f83abf6d8a8fd5e37ffbe9
Sandm <9277206@qq.com>
2019-01-08 17:41:24 +0800

    创建数一数activity

--- app/src/main/java/cc/auuo/child/FullscreenActivity.kt
+++ app/src/main/java/cc/auuo/child/FullscreenActivity.kt
@@ -3,7 +3,10 @@ package cc.auuo.child
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import cc.auuo.child.R.layout.activity_fullscreen
+import cc.auuo.child.activity.CountGameActivity
 import cc.auuo.child.util.fullScreen
+import kotlinx.android.synthetic.main.activity_fullscreen.*
+import org.jetbrains.anko.startActivity
 
 class FullscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,12 @@ class FullscreenActivity : AppCompatActivity() {
         setContentView(activity_fullscreen)
 
         fullScreen()
+        initListener()
+    }
+
+    private fun initListener() {
+        countGameButton.setOnClickListener {
+            startActivity<CountGameActivity>()
+        }
     }
 }
---
+++ app/src/main/java/cc/auuo/child/activity/CountGameActivity.kt
@@ -0,0 +1,13 @@
+package cc.auuo.child.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import cc.auuo.child.R
+
+class CountGameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_count_game)
+    }
+}
