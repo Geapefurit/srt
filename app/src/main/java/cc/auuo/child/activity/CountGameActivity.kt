30857947880fb4062b6c8497915ef2bf575f2ea7
Sandm <9277206@qq.com>
2019-01-08 17:43:56 +0800

    数一数activity全屏显示

--- app/src/main/java/cc/auuo/child/activity/CountGameActivity.kt
+++ app/src/main/java/cc/auuo/child/activity/CountGameActivity.kt
@@ -3,11 +3,14 @@ package cc.auuo.child.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import cc.auuo.child.R
+import cc.auuo.child.util.fullScreen
 
 class CountGameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_count_game)
+
+        fullScreen()
     }
 }
