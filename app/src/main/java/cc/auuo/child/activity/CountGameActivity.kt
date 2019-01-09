151ce0a83fa29fb5735ff9b799714f526f9501bc
Sandm <9277206@qq.com>
2019-01-09 19:15:30 +0800

    feat(count_game): 添加关闭按钮按下效果

--- app/src/main/java/cc/auuo/child/activity/CountGameActivity.kt
+++ app/src/main/java/cc/auuo/child/activity/CountGameActivity.kt
@@ -2,6 +2,7 @@ package cc.auuo.child.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
 import cc.auuo.child.R
 import cc.auuo.child.util.fullScreen
 import kotlinx.android.synthetic.main.activity_count_game.*
@@ -14,11 +15,23 @@ class CountGameActivity : AppCompatActivity() {
 
         fullScreen()
         initListener()
+        initTouch()
     }
 
     private fun initListener() {
         closeButton.setOnClickListener {
             this.finish()
+        }
+    }
+
+    private fun initTouch() {
+        closeButton.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                v.setBackgroundResource(R.drawable.close_button_press)
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                v.setBackgroundResource(R.drawable.close_button)
+            }
+            return@setOnTouchListener false
         }
     }
 }
