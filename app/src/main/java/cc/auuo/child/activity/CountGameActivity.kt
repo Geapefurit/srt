108726ae7d736771a9e4cf3a1887a6cfb1a94f8b
Sandm <9277206@qq.com>
2019-01-09 19:00:48 +0800

    feat(count_game): 添加关闭按钮

--- app/src/main/java/cc/auuo/child/activity/CountGameActivity.kt
+++ app/src/main/java/cc/auuo/child/activity/CountGameActivity.kt
@@ -1,9 +1,10 @@
 package cc.auuo.child.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import cc.auuo.child.R
 import cc.auuo.child.util.fullScreen
+import kotlinx.android.synthetic.main.activity_count_game.*
 
 class CountGameActivity : AppCompatActivity() {
 
@@ -12,5 +13,12 @@ class CountGameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_count_game)
 
         fullScreen()
+        initListener()
+    }
+
+    private fun initListener() {
+        closeButton.setOnClickListener {
+            this.finish()
+        }
     }
 }
