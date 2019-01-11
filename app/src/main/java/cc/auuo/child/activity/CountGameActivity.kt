dbc9dd60c77d5b3cae313aba3acba9b1c0a14e39
Sandm <9277206@qq.com>
2019-01-11 18:59:00 +0800

    feat(count_game): 完成游戏逻辑

--- app/src/main/java/cc/auuo/child/activity/CountGameActivity.kt
+++ app/src/main/java/cc/auuo/child/activity/CountGameActivity.kt
@@ -1,21 +1,29 @@
 package cc.auuo.child.activity
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
 import android.widget.SimpleAdapter
 import cc.auuo.child.R
 import cc.auuo.child.util.fullScreen
 import cc.auuo.child.util.setPressStyle
 import kotlinx.android.synthetic.main.activity_count_game.*
+import org.jetbrains.anko.toast
 import java.util.*
 
 class CountGameActivity : AppCompatActivity() {
 
     private val random = Random()
+    private val handler = Handler()
+    private var answer = 0
 
     companion object {
-        val drawableIds = arrayOf(R.drawable.bear, R.drawable.bicycle, R.drawable.bug, R.drawable.butterfly,
+        private val DRAWABLE_ID = arrayOf(R.drawable.bear, R.drawable.bicycle, R.drawable.bug, R.drawable.butterfly,
                 R.drawable.dog, R.drawable.grape, R.drawable.guitar, R.drawable.mango, R.drawable.umbrella)
+
+        private const val DELAY = 2300L
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,39 +38,74 @@ class CountGameActivity : AppCompatActivity() {
     }
 
     private fun initData() {
-        // init gridView
         getQuestion { answer, resId ->
-            gridView.numColumns = answer
-            if (answer > 3) {
-                gridView.numColumns = if (answer % 2 == 0) answer / 2 else answer / 2 + 1
-            }
-            val data = ArrayList<Map<String, Any>>()
-            for (i in 0 until answer) {
-                val map = HashMap<String, Any>()
-                map["img"] = resId
-                data.add(map)
-            }
-            val adapter = SimpleAdapter(this, data, R.layout.count_game_item, arrayOf("img"), intArrayOf(R.id.countGameItemImg))
-            gridView.adapter = adapter
-
-            // todo init answer button
-            firstAnswerButton.text = "8"
-            secondAnswerButton.text = "6"
-            thirdAnswerButton.text = "7"
+            this.answer = answer
+            setGrid(answer, resId)
+            setAnswerButton(answer)
         }
     }
 
     private fun getQuestion(callback: (Int, Int) -> Unit) {
         // 以后可能从网络获取
         val target = random.nextInt(10) + 1
-        val resId = drawableIds[random.nextInt(drawableIds.size)]
+        val resId = DRAWABLE_ID[random.nextInt(DRAWABLE_ID.size)]
+
         callback(target, resId)
+    }
+
+    private fun setGrid(count: Int, resId: Int) {
+        gridView.numColumns = count
+        if (count > 3) {
+            gridView.numColumns = if (count % 2 == 0) count / 2 else count / 2 + 1
+        }
+        val data = ArrayList<Map<String, Any>>()
+        for (i in 0 until count) {
+            val map = HashMap<String, Any>()
+            map["img"] = resId
+            data.add(map)
+        }
+        val adapter = SimpleAdapter(this, data, R.layout.count_game_item, arrayOf("img"), intArrayOf(R.id.countGameItemImg))
+        gridView.adapter = adapter
+    }
+
+    private fun setAnswerButton(answer: Int) {
+        val answers = IntArray(3)
+        val nums = (1..10).filter { it != answer }.toIntArray().shuffle()
+        answers[0] = answer
+        answers[1] = nums[0]
+        answers[2] = nums[1]
+
+        answers.shuffle()
+
+        firstAnswerButton.text = answers[0].toString()
+        secondAnswerButton.text = answers[1].toString()
+        thirdAnswerButton.text = answers[2].toString()
     }
 
     private fun initListener() {
         closeButton.setOnClickListener {
             this.finish()
         }
+
+        firstAnswerButton.setOnClickListener(::verifyAnswer)
+        secondAnswerButton.setOnClickListener(::verifyAnswer)
+        thirdAnswerButton.setOnClickListener(::verifyAnswer)
+    }
+
+    private fun verifyAnswer(view: View) {
+        val buttonText = (view as Button).text
+        if (buttonText == answer.toString()) {
+            toast("答案正确")
+            reloadDataLater()
+        } else {
+            toast("答案错误")
+        }
+    }
+
+    private fun reloadDataLater() {
+        handler.postDelayed({
+            initData()
+        }, DELAY)
     }
 
     private fun initTouch() {
@@ -78,5 +121,15 @@ class CountGameActivity : AppCompatActivity() {
         thirdAnswerButton.setOnTouchListener { v, event ->
             setPressStyle(v, event, R.drawable.button_blue, R.drawable.button_blue_press)
         }
+    }
+
+    private fun IntArray.shuffle(): IntArray {
+        for (i in 0 until this.size) {
+            val randomIndex = random.nextInt(this.size)
+            val t = this[i]
+            this[i] = this[randomIndex]
+            this[randomIndex] = t
+        }
+        return this
     }
 }
